8cbd1278a8a39fc37416b791de0096bbcd317530
ParichayNITW <parichay.nitwarangal@gmail.com>
2025-06-20 01:04:15 +0530

    Update pipeline_optimization_app.py

--- pipeline_optimization_app.py
+++ pipeline_optimization_app.py
@@ -2063,6 +2063,6 @@ with tab_sim:
 
 st.markdown("""
     <div style='text-align:center;color:#888;margin-top:1.1em;'>
-    st.markdown("&copy; 2025 Pipeline Optima™ v1.1.1. Developed by Parichay Das.", unsafe_allow_html=True)
+        &copy; 2025 Pipeline Optima™ v1.1.1. Developed by Parichay Das.
     </div>
 """, unsafe_allow_html=True)
