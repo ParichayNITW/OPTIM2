18d2a5dfe11378c19e4f6ae9cfad1f9780e4bc56
ParichayNITW <parichay.nitwarangal@gmail.com>
2025-07-23 13:03:52 +0530

    Update pipeline_model.py

--- pipeline_model.py
+++ pipeline_model.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 
 # -------- NEOS email --------
-os.environ['NEOS_EMAIL'] = os.environ.get('NEOS_EMAIL', 'your@email.com')
+os.environ['NEOS_EMAIL'] = os.environ.get('NEOS_EMAIL', 'parichay.nitwarangal@gmail.com')
 
 # -------- DRA Curve Data Loader --------
 DRA_CSV_FILES = {
