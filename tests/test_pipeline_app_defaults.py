d6e3694f5d6d289a81ae32415819f9824bcc1663
ParichayNITW <parichay.nitwarangal@gmail.com>
2025-11-17 10:03:27 +0530

    Raise default DRA step to 5

--- tests/test_pipeline_app_defaults.py
+++ tests/test_pipeline_app_defaults.py
@@ -36,7 +36,7 @@ def test_collect_search_depth_kwargs_handles_missing_pipeline_constants(monkeypa
 
     assert defaults == {
         "rpm_step": 25,
-        "dra_step": 2,
+        "dra_step": 5,
         "coarse_multiplier": 5.0,
         "state_top_k": 50,
         "state_cost_margin": 5000.0,
