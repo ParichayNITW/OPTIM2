8099e39f04e06aa3251bba5e7b3f5ce78d62ee55
ParichayNITW <parichay.nitwarangal@gmail.com>
2025-12-03 23:23:06 +0530

    Update test_pipeline_app_defaults.py

--- tests/test_pipeline_app_defaults.py
+++ tests/test_pipeline_app_defaults.py
@@ -13,9 +13,6 @@ def test_collect_search_depth_kwargs_handles_missing_pipeline_constants(monkeypa
     tracked_keys = [
         "search_rpm_step",
         "search_dra_step",
-        "search_coarse_multiplier",
-        "search_state_top_k",
-        "search_state_cost_margin",
     ]
     sentinel = object()
     previous_values = {}
@@ -37,7 +34,5 @@ def test_collect_search_depth_kwargs_handles_missing_pipeline_constants(monkeypa
     assert defaults == {
         "rpm_step": 25,
         "dra_step": 2,
-        "coarse_multiplier": 5.0,
-        "state_top_k": 50,
-        "state_cost_margin": 5000.0,
+        "collect_state_audit": True,
     }
